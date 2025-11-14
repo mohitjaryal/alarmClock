a8e7072b6a68fc40066d53935b25f77f4ad6c1a1
Mohit <mohitt.tech@gmail.com>
2025-11-14 19:20:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,7 @@ print('Simple alarm clock !')
 # User input
 alarm_time = int(input('Enter time : (HH:MM:SS)')) # hour:minute:second
 
+# Extract hour, minute, second
+alarm_hour = int(alarm_time[0:2])
+alarm_minute = int(alarm_time[3:5])
+alarm_second = int(alarm_time[6:8])
